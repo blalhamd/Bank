d344483b27b2ed238eae4d57d2da4017689ee482
blalhamd <141357295+blalhamd@users.noreply.github.com>
2023-08-03 21:22:02 +0300

    Add files via upload

---
+++ Entities/CustomerTransaction.cs
@@ -0,0 +1,11 @@
+﻿namespace BankSystem.Entities
+{
+    public class CustomerTransaction
+    {
+        public Customer? Customer { get; set; }
+        public Transaction? transaction { get; set; }
+        public int transactionId { get; set; }
+        public int customerId { get; set; }
+
+    }
+}
